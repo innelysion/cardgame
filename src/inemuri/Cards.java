403493e46874dcd14e70fc99b73f84a2062aabcc
game01 <game01@dell2016g043>
2017-06-30 12:11:36 +0900

    heyheyhey

--- src/inemuri/Cards.java
+++ src/inemuri/Cards.java
@@ -1,10 +1,12 @@
 package inemuri;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Cards {
 
 	private String name; // 名称s
+	private int rarity; // 稀有度
 	private String imageName; // 卡牌图片名
 	private String ruleText; // 卡牌说明文
 	private Family family; // 基础类型
@@ -13,7 +15,10 @@ public class Cards {
 	private int id; // 序号
 	private int cost; // 消耗
 	private int value; // 价值
-	private int rarity; // 稀有度
+
+	private boolean isLocked;
+	private boolean isReversed;
+	private boolean isPhantom;
 
 	// TODO 看看怎么将自身的处理逻辑传给外面
 	// private ArrayList<> //判断条件容器
@@ -21,6 +26,7 @@ public class Cards {
 
 	Cards() {
 		name = "";
+		rarity = 0;
 		imageName = "";
 		ruleText = "";
 		family = Family.DEFAULT;
@@ -30,8 +36,203 @@ public class Cards {
 		id = 0;
 		cost = 0;
 		value = 0;
-		rarity = 0;
+
+		isReversed = false;
 	}
+
+	Cards(int index) {
+		this();
+		id = index;
+		loadCardData(id);
+	}
+
+	private void loadCardData(int index) {
+		CardData data = new CardData();
+		name = data.tempdata_name.get(index);
+		rarity = data.tempdata_rarity.get(index);
+		// imageName = data.tempdata_imageName.get(index).;
+		ruleText = data.tempdata_ruleText.get(index);
+		family = data.tempdata_family.get(index);
+		types = data.tempdata_types.get(index);
+		cost = data.tempdata_cost.get(index);
+		value = data.tempdata_value.get(index);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	private static class CardData {
+		HashMap<Integer, String> tempdata_name = new HashMap<Integer, String>();
+		HashMap<Integer, Integer> tempdata_rarity = new HashMap<Integer, Integer>();
+		// HashMap<Integer, String> tempdata_imageName = new HashMap<Integer,
+		// String>();
+		HashMap<Integer, String> tempdata_ruleText = new HashMap<Integer, String>();
+		HashMap<Integer, Family> tempdata_family = new HashMap<Integer, Family>();
+		HashMap<Integer, ArrayList<Types>> tempdata_types = new HashMap<Integer, ArrayList<Types>>();
+		HashMap<Integer, Integer> tempdata_value = new HashMap<Integer, Integer>();
+		HashMap<Integer, Integer> tempdata_cost = new HashMap<Integer, Integer>();
+
+		CardData() {
+			String element = "灵魔妖神幻源心无";
+			int start = 1;
+			int end = 9;
+			int tid = 0;
+			for (int i = start; i < end; i++) {
+				tempdata_name.put(i, element.substring(i - 1, i) + "1");
+				tempdata_rarity.put(i, 1);
+				// tempdata_imageName
+				tempdata_ruleText.put(i, element.substring(i - 1, i) + "1");
+				tempdata_family.put(i, Family.ACTIONS);
+				tempdata_types.put(i, new ArrayList<Types>() {
+					{
+						add(Types.ATTACK);
+					}
+				});
+				tempdata_value.put(i, 1);
+				tempdata_cost.put(i, 1);
+			}
+
+			//////////////////////////////////
+			tid = 9;
+			tempdata_name.put(tid, "盾1");
+			tempdata_rarity.put(tid, 1);
+			// tempdata_imageName
+			tempdata_ruleText.put(tid, "盾1");
+			tempdata_family.put(tid, Family.ACTIONS);
+			tempdata_types.put(tid, new ArrayList<Types>() {
+				{
+					add(Types.DEFENCE);
+				}
+			});
+			tempdata_value.put(tid, 1);
+			tempdata_cost.put(tid, 1);
+
+			//////////////////////////////////
+			tid = 10;
+			tempdata_name.put(tid, "集1");
+			tempdata_rarity.put(tid, 1);
+			// tempdata_imageName
+			tempdata_ruleText.put(tid, "集1");
+			tempdata_family.put(tid, Family.ACTIONS);
+			tempdata_types.put(tid, new ArrayList<Types>() {
+				{
+					add(Types.FOCUS);
+				}
+			});
+			tempdata_value.put(tid, 1);
+			tempdata_cost.put(tid, 1);
+
+			//////////////////////////////////
+			tid = 11;
+			tempdata_name.put(tid, "二重结界");
+			tempdata_rarity.put(tid, 1);
+			// tempdata_imageName
+			tempdata_ruleText.put(tid, "盾结算+1");
+			tempdata_family.put(tid, Family.EFFECTS);
+			tempdata_types.put(tid, new ArrayList<Types>() {
+				{
+					add(Types.SEAL);
+				}
+			});
+			tempdata_value.put(tid, 1);
+			tempdata_cost.put(tid, 1);
+
+			//////////////////////////////////
+			tid = 12;
+			tempdata_name.put(tid, "御币");
+			tempdata_rarity.put(tid, 1);
+			// tempdata_imageName
+			tempdata_ruleText.put(tid, "灵1盾1");
+			tempdata_family.put(tid, Family.EFFECTS);
+			tempdata_types.put(tid, new ArrayList<Types>() {
+				{
+					add(Types.EQUIP);
+				}
+			});
+			tempdata_value.put(tid, 1);
+			tempdata_cost.put(tid, 1);
+
+			//////////////////////////////////
+			tid = 13;
+			tempdata_name.put(tid, "塞钱征收");
+			tempdata_rarity.put(tid, 1);
+			// tempdata_imageName
+			tempdata_ruleText.put(tid, "下回合抽牌阶段额外抽两张牌");
+			tempdata_family.put(tid, Family.EVENTS);
+			tempdata_types.put(tid, new ArrayList<Types>() {
+				{
+					add(Types.EVENT);
+				}
+			});
+			tempdata_value.put(tid, 1);
+			tempdata_cost.put(tid, 1);
+
+
+			//////////////////////////////////
+			tid = 14;
+			tempdata_name.put(tid, "迷你八卦炉");
+			tempdata_rarity.put(tid, 1);
+			// tempdata_imageName
+			tempdata_ruleText.put(tid, "魔2");
+			tempdata_family.put(tid, Family.EFFECTS);
+			tempdata_types.put(tid, new ArrayList<Types>() {
+				{
+					add(Types.EQUIP);
+				}
+			});
+			tempdata_value.put(tid, 1);
+			tempdata_cost.put(tid, 1);
+
+			//////////////////////////////////
+			tid = 15;
+			tempdata_name.put(tid, "魔力增幅");
+			tempdata_rarity.put(tid, 1);
+			// tempdata_imageName
+			tempdata_ruleText.put(tid, "魔结算+1");
+			tempdata_family.put(tid, Family.EFFECTS);
+			tempdata_types.put(tid, new ArrayList<Types>() {
+				{
+					add(Types.MAGIC);
+				}
+			});
+			tempdata_value.put(tid, 1);
+			tempdata_cost.put(tid, 1);
+
+			//////////////////////////////////
+			tid = 16;
+			tempdata_name.put(tid, "魔力吸收");
+			tempdata_rarity.put(tid, 1);
+			// tempdata_imageName
+			tempdata_ruleText.put(tid, "如被敌方先手攻击命中且敌方本回合内使用过【魔】的话，魔结算+3");
+			tempdata_family.put(tid, Family.EFFECTS);
+			tempdata_types.put(tid, new ArrayList<Types>() {
+				{
+					add(Types.MAGIC);
+				}
+			});
+			tempdata_value.put(tid, 1);
+			tempdata_cost.put(tid, 1);
+
+			//////////////////////////////////
+			tid = 17;
+			tempdata_name.put(tid, "蛇蛙发饰");
+			tempdata_rarity.put(tid, 1);
+			// tempdata_imageName
+			tempdata_ruleText.put(tid, "盾1集1，从手牌进入弃牌区时，己方全体恢复10点HP");
+			tempdata_family.put(tid, Family.EFFECTS);
+			tempdata_types.put(tid, new ArrayList<Types>() {
+				{
+					add(Types.EQUIP);
+				}
+			});
+			tempdata_value.put(tid, 1);
+			tempdata_cost.put(tid, 1);
+
+
+		}
+	}
+
 }
 
 // 基础类型
--- src/inemuri/GameManager.java
+++ src/inemuri/GameManager.java
@@ -2,31 +2,35 @@ package inemuri;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
 
 public class GameManager {
 
-	private String deck;
-	private ArrayList<Character> library, hand, stack, discarded, exiled;
+	//private String deck = "1,15;";
+	private ArrayList<Cards> library, hand, stack, discarded, exiled;
 
 	private int drawCardTimes = 2;
 	private int HP = 50;
 	private int ATK = 5;
 
 	GameManager() {
-		deck = "剑剑剑剑剑剑剑剑剑剑盾盾盾盾盾集集集集集斧斧斧斧斧斧斧斧斧斧盾盾盾盾盾集集集集集枪枪枪枪枪枪枪枪枪枪盾盾盾盾盾集集集集集";
-		library = new ArrayList<Character>();
-		hand = new ArrayList<Character>();
-		stack = new ArrayList<Character>();
-		discarded = new ArrayList<Character>();
-		exiled = new ArrayList<Character>();
+		library = new ArrayList<Cards>();
+		hand = new ArrayList<Cards>();
+		stack = new ArrayList<Cards>();
+		discarded = new ArrayList<Cards>();
+		exiled = new ArrayList<Cards>();
 
-		for (char c : deck.toCharArray()) {
-			library.add(c);
+
+		for (int i = 1; i < 18; i++)
+		{
+			library.add(new Cards(i));
+			library.add(new Cards(i));
+			library.add(new Cards(i));
 		}
 		shuffle(library);
+
+		for (Cards c : library){
+			System.out.print(c.getName() + ",");
+		}
 	}
 
 	public void mainUpdate() {
@@ -39,7 +43,6 @@ public class GameManager {
 				}
 				hand.add(library.remove(0));
 			}
-			Collections.sort(hand);
 			printdebug();
 		}
 		if (Input.keyRe.X) {
@@ -56,34 +59,18 @@ public class GameManager {
 	}
 
 	private void printdebug() {
-		System.out.println("\r\n\r\n\r\n\r\n\r\n\r\n");
-		System.out.println("牌库:" + library.size() + " " + library);
-		System.out.println("手牌:" + hand.size() + " " + hand);
-		System.out.println("弃牌:" + discarded.size() + " " + discarded);
 
 	}
 
 	private void stackProcess() {
 		if (!stack.isEmpty()) {
-			int pervHP = HP;
 
-			// stack.forEach(c -> this.HP = c == '剑' ? this.HP - 3 : c == '斧' ||
-			// c == '枪' ? this.HP - 1 : this.HP);
-			Predicate<Character> f1 = c -> c == '剑';
-			Predicate<Character> f2 = c -> c == '斧';
-			Predicate<Character> f3 = c -> c == '枪';
-
-			stack.stream().forEach(c -> HP -= f1.test(c) ? 3 : f2.or(f3).test(c) ? 1 : 0);
-
-			System.out.println("打出了:" + stack);
-			System.out.println("对手造成" + (pervHP - HP) + "点伤害　剩余血量:" + HP);
 			discarded.addAll(stack);
 			stack.clear();
 		}
 	}
 
 	private <T> void shuffle(ArrayList<T> list) {
-		System.out.println(list);
 		Collections.shuffle(list);
 	}
 
